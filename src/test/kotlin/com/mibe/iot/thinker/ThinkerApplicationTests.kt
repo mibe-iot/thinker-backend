2859ab6359d53993d77acebd304325d9a38302b1
Ilya Buglakov <neonlightnight@gmail.com>
2021-08-19 15:39:26 +0300

    initial commit

---
+++ src/test/kotlin/com/mibe/iot/thinker/ThinkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mibe.iot.thinker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThinkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
