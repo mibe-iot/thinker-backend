8fe3f2e155a22ad3bdeaf0d5dfb8b03929b7c111
Ilya Buglakov <neonlightnight@gmail.com>
2021-08-25 17:58:06 +0300

    fix: package of Validations.kt, add: doc

--- src/main/kotlin/com/mibe/iot/thinker/validation/application/Validations.kt
+++ src/main/kotlin/com/mibe/iot/thinker/validation/application/Validations.kt
@@ -1,15 +1,19 @@
-package com.mibe.iot.thinker.validation
+package com.mibe.iot.thinker.validation.application
 
 import com.mibe.iot.thinker.validation.domain.ValidationException
 import io.konform.validation.Invalid
 import io.konform.validation.Validation
 import reactor.core.publisher.Mono
 
+/**
+ * Validates valueUnderValidation and returns it as Mono if validation is successful or returns Mono.error if validation
+ * has failed
+ */
 fun <T> mapToErrorMonoIfInvalid(valueUnderValidation: T, validation: Validation<T>): Mono<T> {
     val validationResult = validation.validate(valueUnderValidation)
     return if (validationResult is Invalid<*>) {
         Mono.error(ValidationException(validationResult.errors))
     } else {
-        Mono.just(valueUnderValidation)
+        Mono.just(valueUnderValidation!!)
     }
 }
