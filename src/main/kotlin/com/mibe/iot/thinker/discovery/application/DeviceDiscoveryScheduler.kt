b4ac7ccce67510f3770d0bfc2a3eeb7ea2d5cf85
Ilya Buhlakou <neonlightnight@gmail.com>
2022-02-12 13:38:58 +0300

    fix: discovery scheduler interrupted exception on exit

--- src/main/kotlin/com/mibe/iot/thinker/discovery/application/DeviceDiscoveryScheduler.kt
+++ src/main/kotlin/com/mibe/iot/thinker/discovery/application/DeviceDiscoveryScheduler.kt
@@ -1,12 +1,19 @@
 package com.mibe.iot.thinker.discovery.application
 
 import com.mibe.iot.thinker.discovery.application.port.from.ControlDeviceDiscoveryPort
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import javax.annotation.PreDestroy
+import kotlin.coroutines.coroutineContext
 
 @Component
 class DeviceDiscoveryScheduler
@@ -14,6 +21,7 @@ class DeviceDiscoveryScheduler
     private val controlDeviceDiscoveryUseCase: ControlDeviceDiscoveryPort
 ) {
     private val log = KotlinLogging.logger{}
+    private var currentJob: Job? = null
 
     /**
      * This job will periodically start discovery for some time to update discovered devices.
@@ -23,14 +31,21 @@ class DeviceDiscoveryScheduler
     @Scheduled(cron = "0 * * * * *")
     fun runPeriodicalDiscovery() {
         if(controlDeviceDiscoveryUseCase.isDiscovering()) return
-        log.debug { "Periodical device discovery started" }
+        log.info { "Periodical device discovery started" }
         runBlocking {
-            controlDeviceDiscoveryUseCase.startDiscovery()
-            delay(10_000)
-            //Won't shut down discovery if it was run by other component
-            controlDeviceDiscoveryUseCase.stopDiscovery(gracefully = true)
+            currentJob = launch {
+                controlDeviceDiscoveryUseCase.startDiscovery()
+                delay(10_000)
+                //Won't shut down discovery if it was run by other component
+                controlDeviceDiscoveryUseCase.stopDiscovery(gracefully = true)
+            }
         }
         log.debug { "Periodical device discovery ended" }
+    }
+
+    @PreDestroy
+    fun cancelJobBeforeDestroy() {
+        currentJob?.cancel()
     }
 
 }
