3180e8eb34e0fe030f7fe45a4ac3cbb37ae3b5c3
Ilya Buglakov <neonlightnight@gmail.com>
2022-05-07 22:54:00 +0300

    fix: remove old settings implementation

--- thinker-persistence/src/main/kotlin/com/mibe/iot/thinker/persistence/repository/SpringDataConfigurationRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.mibe.iot.thinker.persistence.repository
-
-import com.mibe.iot.thinker.persistence.entity.ConfigurationEntity
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-
-interface SpringDataConfigurationRepository : ReactiveMongoRepository<ConfigurationEntity, String>
