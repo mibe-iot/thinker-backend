8c370be3efa6effc544deac5c49c14de0e00b8e1
Ilya Buglakov <neonlightnight@gmail.com>
2022-06-07 00:14:39 +0300

    feat: add cmd runner

--- src/main/kotlin/com/mibe/iot/thinker/app/ThinkerApplication.kt
+++ src/main/kotlin/com/mibe/iot/thinker/app/ThinkerApplication.kt
@@ -4,8 +4,11 @@ import com.mibe.iot.thinker.domain.data.EmailAddress
 import com.mibe.iot.thinker.domain.hooks.Hook
 import com.mibe.iot.thinker.domain.hooks.SendEmailHook
 import com.mibe.iot.thinker.persistence.PersistenceBeanScanMarker
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -14,7 +17,16 @@ import kotlin.reflect.KClass
 @SpringBootApplication
 @EnableScheduling
 @EnableReactiveMongoRepositories(basePackageClasses = [PersistenceBeanScanMarker::class])
-class ThinkerApplication
+class ThinkerApplication {
+
+    @Value("\${MONGO_INITDB_ROOT_USERNAME}")
+    lateinit var aaa: String
+
+    @Bean
+    fun cmd() = CommandLineRunner {
+        println(aaa)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ThinkerApplication>(*args)
