4a701a911f7e5f418b5f6df6a10260fb980dde85
Ilya Buglakov <neonlightnight@gmail.com>
2022-06-06 20:28:27 +0300

    feat(discovery): add discovered device duration

--- src/main/kotlin/com/mibe/iot/thinker/app/discovery/GetDiscoveredDeviceService.kt
+++ src/main/kotlin/com/mibe/iot/thinker/app/discovery/GetDiscoveredDeviceService.kt
@@ -4,8 +4,11 @@ import com.mibe.iot.thinker.service.discovery.port.GetDiscoveredDevicePort
 import com.mibe.iot.thinker.service.discovery.GetDiscoveredDeviceUseCase
 import com.mibe.iot.thinker.domain.discovery.DiscoveredDevice
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Service
 class GetDiscoveredDeviceService
@@ -14,6 +17,8 @@ class GetDiscoveredDeviceService
 ) : GetDiscoveredDeviceUseCase {
 
     override suspend fun getDiscoveredDevices(): Flow<DiscoveredDevice> {
+        val now = LocalDateTime.now();
         return getDiscoveredDevicePort.getDiscoveredDevices()
+            .filter { Duration.between(it.discoveredAt, now) < Duration.ofMinutes(5) }
     }
 }
